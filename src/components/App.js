90ae43aeda2041ca230489bab1a0d405edc7520a
k-hongkham <k.hongkham@gmail.com>
2022-09-14 15:17:28 -0400

    initial commit.

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import "../style/App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return <Router></Router>;
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { App } from "./components";
+import "./style/index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
